dba8c1a699d5769c36040ae2dc085ef4e0d1c267
Eagle Cheow <eagle.cheow@hotmail.com>
2017-12-20 15:04:34 +0800

    Added more reject cases and added spreadsheet style

--- SCPDRecorder/Program.cs
+++ SCPDRecorder/Program.cs
@@ -13,26 +13,49 @@ namespace SCPDRecorder
 
         static void Main(string[] args)
         {
+            Console.WriteLine("===================");
+            Console.WriteLine("s-CPD Recorder v1.0");
+            Console.WriteLine("===================");
+            Console.WriteLine();
+            Console.WriteLine("Enter 'Q' to stop recording");
+            Console.WriteLine();
             RecordNumber();
             ExportExcel();
+            Console.WriteLine("Press any key to quit...");
+            Console.ReadKey();
         }
 
+        /// <summary>
+        /// This function exports the list to an excel file
+        /// </summary>
         static void ExportExcel()
         {
+            Console.WriteLine("Exporting data to excel...");
+
             var excel = new Microsoft.Office.Interop.Excel.Application();
             excel.Visible = true;
             var workbook = excel.Workbooks.Add(Type.Missing);
             var sheet = (Microsoft.Office.Interop.Excel.Worksheet)workbook.ActiveSheet;
             sheet.Name = "s-CPD Record";
+            sheet.Cells[1, 1] = "s-CPD Record List";
 
-            int startIndex = 1;
+            sheet.Range[sheet.Cells[1, 1], sheet.Cells[1, 2]].Merge();
+
+            sheet.Cells[1, 1].Font.Bold = true;
+
+            int startIndex = 2;
             foreach (string libraryNumber in libraryNumberList)
             {
                 sheet.Cells[startIndex, 1] = libraryNumber;
                 startIndex++;
             }
+
+            sheet.Columns.AutoFit();
         }
 
+        /// <summary>
+        /// Records valid library numbers to list
+        /// </summary>
         static void RecordNumber()
         {
             bool exitStatus = false;
@@ -42,7 +65,11 @@ namespace SCPDRecorder
                 string libraryNumber = Console.ReadLine();
                 if (!libraryNumber.Contains('q') && !libraryNumber.Contains('Q'))
                 {
-                    if (!libraryNumberList.Contains(libraryNumber))
+                    if (string.IsNullOrEmpty(libraryNumber) || !libraryNumber.Any(char.IsDigit))
+                    {
+                        Console.WriteLine("Invalid data entered, please try again");
+                    }
+                    else if (!libraryNumberList.Contains(libraryNumber))
                     {
                         libraryNumberList.Add(libraryNumber);
                         Console.WriteLine("Library number registered");
@@ -56,6 +83,8 @@ namespace SCPDRecorder
                 {
                     exitStatus = true;
                 }
+
+                Console.WriteLine();
             }
         }
     }
